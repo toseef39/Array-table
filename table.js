9cf5a37a3ce6777ef7326abe1432f9eec8ff6aa3
Touseef ur Rehman <toseefkalwar2001@gmail.com>
2024-09-28 10:45:27 +0500

    Add files via upload

---
+++ table.js
@@ -0,0 +1,32 @@
+let table = +prompt("Enter Table Number")
+let num = 5;
+for (let i = 0; i <= 10; i++) {
+    if (table == 0){
+        console.log(num + "x" + i + "=" + i * num)
+    } else{
+        console.log(table + "X" + i + "=" + i * table)
+    }
+}
+
+// let array1 = [1, 2, 3, 4, 5]
+//   array1.shift()
+//  console.log (array1)
+// let array1 = [1, 2, 3, 4, 5]
+//   array1.unshift(5)
+//  console.log (array1)
+// let array1 = [1, 2, 3, 4, 5]
+//   array1.pop()
+//  console.log (array1)
+// let array1 = [1, 2, 3, 4, 5]
+//   array1.push(8)
+//  console.log (array1)
+// let array1 = [1, 2, 3, 4, 5]
+//   array1.splice(1,2, "come", "go")
+//  console.log (array1)
+// let array1 = [1, 2, 3, 4, 5]
+//   array1.slice(0, 4)
+//  console.log (array1)
+// let array1 = [1, 2, 3, 4, 5]
+//     array2 =[2, 3,8, 9, 5]
+// //   array1.shift()
+//  console.log (array1.concat(array2))
